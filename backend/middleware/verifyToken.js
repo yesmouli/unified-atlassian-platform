2b33d0b833585546098889435bf99df712e19af4
yesmouli <yesmouli@gmail.com>
2024-09-13 16:29:31 +0530

    Create verifyToken.js

---
+++ backend/middleware/verifyToken.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+// Middleware to verify JWT token
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(403).json({ message: 'No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+module.exports = verifyToken;
