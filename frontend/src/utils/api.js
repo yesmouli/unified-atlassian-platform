f46f58ddf5f87af930e93c41260a33957eb09f05
yesmouli <yesmouli@gmail.com>
2024-09-13 16:23:28 +0530

    Create api.js

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default api;
