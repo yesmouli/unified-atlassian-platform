2983118fe15a05c5741fb8e96e4cb880978a6b6d
yesmouli <yesmouli@gmail.com>
2024-09-13 16:23:55 +0530

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from '../components/Dashboard';
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <Dashboard />
+    </div>
+  );
+};
+
+export default Home;
